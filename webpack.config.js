825757c9b429f8db728b7c069c1308a8ed7f94a0
ArtyomDev <yapryntsev.a02@mail.ru>
2022-06-09 19:13:40 +0500

    Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry:  './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      }
+    ],
+  },
+  devServer: {
+    hot: false
+  },
+};
