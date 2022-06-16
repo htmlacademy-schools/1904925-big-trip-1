34f7d505fcc6441abff15bc25035e8fbd2d028eb
Lera100022 <lera.102.str@gmail.com>
2022-06-16 14:02:16 +0500

    6.5.1

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,22 +1,47 @@
 import dayjs from 'dayjs';
+import {offers} from './offers';
+import {destinations} from './destinations';
+import {nanoid} from 'nanoid';
 
-export const sortTaskByDay = (pointA, pointB) => dayjs(pointA.startDate).diff(dayjs(pointB.startDate));
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
 
-export const sortTaskByDuration = (pointA, pointB) => {
-  const durationPointA = dayjs(pointA.endDate).diff(dayjs(pointA.startDate));
-  const durationPointB = dayjs(pointB.endDate).diff(dayjs(pointB.startDate));
-
-  if (durationPointB - durationPointA !== 0) {
-    return durationPointB - durationPointA;
-  } else {
-    return dayjs(pointA.startDate).diff(dayjs(pointB.startDate));
-  }
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const sortTaskByPrice = (pointA, pointB) => {
-  if (pointB.price - pointA.price !== 0) {
-    return pointB.price - pointA.price;
-  } else {
-    return dayjs(pointA.startDate).diff(dayjs(pointB.startDate));
-  }
+const generatePrice = () => getRandomInteger(1, 100) * 10;
+
+const generateFromToDates = () => {
+  const maxGap = 14;
+  const fromDate = dayjs()
+    .add(getRandomInteger(-maxGap, maxGap), 'day')
+    .add(getRandomInteger(-maxGap, maxGap), 'hour')
+    .add(getRandomInteger(-maxGap, maxGap), 'minute');
+  const toDate = fromDate
+    .clone()
+    .add(getRandomInteger(0, 14), 'day')
+    .add(getRandomInteger(0, 59), 'hour')
+    .add(getRandomInteger(0, 59), 'minute');
+  return {
+    from: fromDate.toISOString(),
+    to: toDate.toISOString()
+  };
+};
+
+export const generatePoint = () => {
+  const dates = generateFromToDates();
+  const destinationArray = destinations();
+  const offerArray = offers();
+
+  return {
+    basePrice: generatePrice(),
+    dateFrom: dates.from,
+    dateTo: dates.to,
+    destination: destinationArray[getRandomInteger(0,destinationArray.length-1)],
+    id: nanoid(),
+    isFavorite: Boolean(getRandomInteger(0,1)),
+    offers: offerArray,
+    type: offerArray[getRandomInteger(0,offerArray.length-1)].type
+  };
 };
