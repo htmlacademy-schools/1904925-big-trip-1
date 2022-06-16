bd2aa824228d60def3f6e54f11720e8d3029e25e
Lera100022 <lera.102.str@gmail.com>
2022-06-16 21:33:55 +0500

    7.4.1

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -1,122 +1,188 @@
-import { ADDITIONAL_OPTIONS, WAYPOINTS } from '../utils/constData';
+import { destinations } from "../mock/destinations";
+import { offers } from "../mock/offers";
+import SmartView from "./smart-view";
 import {
-  createEventOptionsMarkup,
-  createEventTypesMarkup,
-} from '../utils/forms';
-import {SmartView} from './smart-view';
+  createPointTypesMarkup,
+  createOffersSectionMarkup,
+} from "../utils/forms";
+import flatpickr from "flatpickr";
+import he from "he";
 
+import "../../node_modules/flatpickr/dist/flatpickr.min.css";
 
-const createEventEditTemplate = (data, citiesList) =>
-  `<form class='event event--edit' action='#' method='post'>
-    <header class='event__header'>
-      <div class='event__type-wrapper'>
-        <label class='event__type  event__type-btn' for='event-type-toggle-1'>
-          <span class='visually-hidden'>Choose event type</span>
-          <img class='event__type-icon' width='17' height='17' src='img/icons/${data.travel.waypoint.toLowerCase()}.png' alt='Event type icon'>
-        </label>
-        <input class='event__type-toggle  visually-hidden' id='event-type-toggle-1' type='checkbox'>
+const createPointEditTemplate = (point) => {
+  const { basePrice: price, destination, type } = point;
 
-        <div class='event__type-list'>
-          <fieldset class='event__type-group'>
-            <legend class='visually-hidden'>Event type</legend>
-            ${createEventTypesMarkup(WAYPOINTS, data.travel.waypoint)}
-          </fieldset>
-        </div>
-      </div>
+  const pointTypeLabel = type.charAt(0).toUpperCase() + type.slice(1);
 
-      <div class='event__field-group  event__field-group--destination'>
-        <label class='event__label  event__type-output' for='event-destination-1'>
-          ${data.travel.waypoint}
-        </label>
-        <input class='event__input  event__input--destination' id='event-destination-1' type='text' name='event-destination' value='${
-          data.travel.city
-        }' list='destination-list-1'>
-        <datalist id='destination-list-1'>
-         ${Object.entries(citiesList)
-           .map((entry) => `<option value='${entry[0]}'></option>`)
-           .join('')}
-        </datalist>
-      </div>
+  const pointTypesMarkup = createPointTypesMarkup(offers(), type);
+  const destinationOptions = destinations()
+    .map((x) => `<option value="${x.name}"></option>`)
+    .join("");
 
-      <div class='event__field-group  event__field-group--time'>
-        <label class='visually-hidden' for='event-start-time-1'>From</label>
-        <input class='event__input  event__input--time' id='event-start-time-1' type='text' name='event-start-time' value='${new Date(
-          data.travel.date.pure
-        ).toLocaleDateString('en-US')} ${data.travel.time.start}'>
-        &mdash;
-        <label class='visually-hidden' for='event-end-time-1'>To</label>
-        <input class='event__input  event__input--time' id='event-end-time-1' type='text' name='event-end-time' value='${new Date(
-          data.travel.date.pure
-        ).toLocaleDateString('en-US')} ${data.travel.time.end}'>
-      </div>
+  const photosMarkup = destination.pictures
+    .map(
+      (x) => `<img class="event__photo" src="${x.src}" alt="${x.description}">`
+    )
+    .join("");
 
-      <div class='event__field-group  event__field-group--price'>
-        <label class='event__label' for='event-price-1'>
-          <span class='visually-hidden'>Price</span>
-          &euro;
-        </label>
-        <input class='event__input  event__input--price' id='event-price-1' type='text' name='event-price' value='${
-          data.price
-        }'>
-      </div>
+  const editedOffersMarkup = createOffersSectionMarkup(offers(), type);
 
-      <button class='event__save-btn  btn  btn--blue' type='submit'>Save</button>
-      <button class='event__reset-btn' type='reset'>Delete</button>
-      <button class='event__rollup-btn' type='button'>
-        <span class='visually-hidden'>Open event</span>
-      </button>
-    </header>
-    <section class='event__details'>
-      ${createEventOptionsMarkup(ADDITIONAL_OPTIONS)}
-    </section>
-
-      <section class='event__section  event__section--destination'>
-        <h3 class='event__section-title  event__section-title--destination'>Destination</h3>
-        <p class='event__destination-description'>
-            ${citiesList[data.travel.city].description}
-        </p>
-      </section>
-    </section>
-  </form>`;
+  return `<li class="trip-events__item">
+              <form class="event event--edit" action="#" method="post">
+                <header class="event__header">
+                  <div class="event__type-wrapper">
+                    <label class="event__type  event__type-btn" for="event-type-toggle-1">
+                      <span class="visually-hidden">Choose event type</span>
+                      <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+                    </label>
+                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+                    <div class="event__type-list">
+                      <fieldset class="event__type-group">
+                        <legend class="visually-hidden">Event type</legend>
+                        ${pointTypesMarkup}
+                      </fieldset>
+                    </div>
+                  </div>
+                  <div class="event__field-group  event__field-group--destination">
+                    <label class="event__label  event__type-output" for="event-destination-1">
+                      ${pointTypeLabel}
+                    </label>
+                    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${he.encode(
+                      destination.name
+                    )}" list="destination-list-1">
+                    <datalist id="destination-list-1">
+                      ${destinationOptions}
+                    </datalist>
+                  </div>
+                  <div class="event__field-group  event__field-group--time">
+                    <label class="visually-hidden" for="event-start-time-1">From</label>
+                    <input class="event__input event__input--time event__input-start-time" id="event-start-time-1" type="text" name="event-start-time" value="">
+                    —
+                    <label class="visually-hidden" for="event-end-time-1">To</label>
+                    <input class="event__input event__input--time event__input-end-time" id="event-end-time-1" type="text" name="event-end-time" value="">
+                  </div>
+                  <div class="event__field-group  event__field-group--price">
+                    <label class="event__label" for="event-price-1">
+                      <span class="visually-hidden">Price</span>
+                      €
+                    </label>
+                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${he.encode(
+                      price.toString()
+                    )}">
+                  </div>
+                  <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+                  <button class="event__reset-btn" type="reset">Delete</button>
+                  <button class="event__rollup-btn" type="button">
+                    <span class="visually-hidden">Open event</span>
+                  </button>
+                </header>
+                <section class="event__details">
+                  ${editedOffersMarkup}
+                  <section class="event__section  event__section--destination">
+                    ${
+                      destination.description
+                        ? '<h3 class="event__section-title  event__section-title--destination">Destination</h3>'
+                        : ""
+                    }
+                    <p class="event__destination-description">${
+                      destination.description ? destination.description : ""
+                    }</p>
+                    <div class="event__photos-container">
+                      <div class="event__photos-tape">
+                        ${photosMarkup}
+                      </div>
+                    </div>
+                  </section>
+                </section>
+              </form>
+            </li>`;
+};
 
 export default class PointEditView extends SmartView {
+  #datepickerFrom = null;
+  #datepickerTo = null;
+
   constructor(point) {
     super();
     this._data = PointEditView.parsePointToData(point);
 
     this.#setInnerHandlers();
+    this.#setDatepicker();
   }
 
   get template() {
-    return createEventEditTemplate(this._data);
+    return createPointEditTemplate(this._data);
   }
+
+  removeElement = () => {
+    super.removeElement();
+
+    if (this.#datepickerFrom) {
+      this.#datepickerFrom.destroy();
+      this.#datepickerFrom = null;
+    }
+    if (this.#datepickerTo) {
+      this.#datepickerTo.destroy();
+      this.#datepickerTo = null;
+    }
+  };
 
   reset = (point) => {
     this.updateData(PointEditView.parsePointToData(point));
   };
 
+  #setDatepicker = () => {
+    this.#datepickerFrom = flatpickr(
+      this.element.querySelector(".event__input-start-time"),
+      {
+        enableTime: true,
+        dateFormat: "d/m/y H:i",
+        defaultDate: this._data.dateFrom,
+        onChange: this.#dateFromChangeHandler,
+      }
+    );
+    this.#datepickerTo = flatpickr(
+      this.element.querySelector(".event__input-end-time"),
+      {
+        enableTime: true,
+        dateFormat: "d/m/y H:i",
+        defaultDate: this._data.dateTo,
+        onChange: this.#dateToChangeHandler,
+      }
+    );
+  };
+
+  #dateFromChangeHandler = ([userDate]) => {
+    this.updateData({
+      dateFrom: userDate.toISOString(),
+    });
+  };
+
+  #dateToChangeHandler = ([userDate]) => {
+    this.updateData({
+      dateTo: userDate.toISOString(),
+    });
+  };
+
   restoreHandlers = () => {
     this.#setInnerHandlers();
+    this.#setDatepicker();
     this.setRollupClickHandler(this._callback.rollupClick);
     this.setFormSubmitHandler(this._callback.formSubmit);
+    this.setDeleteClickHandler(this._callback.deleteClick);
   };
 
   #setInnerHandlers = () => {
     this.element
-      .querySelector('.event__type-group')
-      .addEventListener('change', this.#typeGroupClickHandler);
+      .querySelector(".event__type-group")
+      .addEventListener("change", this.#typeGroupClickHandler);
     this.element
-      .querySelector('.event__input--destination')
-      .addEventListener('change', this.#destinationChangeHandler);
+      .querySelector(".event__input--destination")
+      .addEventListener("change", this.#destinationChangeHandler);
     this.element
-      .querySelector('.event__input-start-time')
-      .addEventListener('change', this.#startTimeChangeHandler);
-    this.element
-      .querySelector('.event__input-end-time')
-      .addEventListener('change', this.#endTimeChangeHandler);
-    this.element
-      .querySelector('.event__input--price')
-      .addEventListener('change', this.#basePriceChangeHandler);
+      .querySelector(".event__input--price")
+      .addEventListener("change", this.#basePriceChangeHandler);
   };
 
   #typeGroupClickHandler = (evt) => {
@@ -139,26 +205,6 @@ export default class PointEditView extends SmartView {
     );
   };
 
-  #startTimeChangeHandler = (evt) => {
-    evt.preventDefault();
-    this.updateData(
-      {
-        dateFrom: evt.target.value,
-      },
-      true
-    );
-  };
-
-  #endTimeChangeHandler = (evt) => {
-    evt.preventDefault();
-    this.updateData(
-      {
-        dateTo: evt.target.value,
-      },
-      true
-    );
-  };
-
   #basePriceChangeHandler = (evt) => {
     evt.preventDefault();
     this.updateData(
@@ -172,8 +218,8 @@ export default class PointEditView extends SmartView {
   setRollupClickHandler = (callback) => {
     this._callback.rollupClick = callback;
     this.element
-      .querySelector('.event__rollup-btn')
-      .addEventListener('click', this.#rollupClickHandler);
+      .querySelector(".event__rollup-btn")
+      .addEventListener("click", this.#rollupClickHandler);
   };
 
   #rollupClickHandler = (evt) => {
@@ -184,20 +230,28 @@ export default class PointEditView extends SmartView {
   setFormSubmitHandler = (callback) => {
     this._callback.formSubmit = callback;
     this.element
-      .querySelector('form')
-      .addEventListener('submit', this.#formSubmitHandler);
+      .querySelector("form")
+      .addEventListener("submit", this.#formSubmitHandler);
   };
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
-    this._callback.formSubmit();
-    this._callback.formSubmit(this._data);
     this._callback.formSubmit(PointEditView.parseDataToPoint(this._data));
   };
 
-  static parsePointToData = (point) => ({
-    ...point,
-  });
+  setDeleteClickHandler = (callback) => {
+    this._callback.deleteClick = callback;
+    this.element
+      .querySelector(".event__reset-btn")
+      .addEventListener("click", this.#formDeleteClickHandler);
+  };
+
+  #formDeleteClickHandler = (evt) => {
+    evt.preventDefault();
+    this._callback.deleteClick(PointEditView.parseDataToPoint(this._data));
+  };
+
+  static parsePointToData = (point) => ({ ...point });
 
   static parseDataToPoint = (data) => {
     const point = { ...data };
@@ -215,7 +269,7 @@ export default class PointEditView extends SmartView {
 
     return {
       description: null,
-      name: '',
+      name: "",
       pictures: [],
     };
   };
