d66570fbb806b8d1b3f3e28b5f65c409a19f6877
Lera100022 <lera.102.str@gmail.com>
2022-06-16 13:38:12 +0500

    5.5.2

--- src/view/site-sort-view.js
+++ src/view/site-sort-view.js
@@ -1,9 +1,10 @@
 import AbstractView from './abstract-view';
+import { SORT_TYPE } from '../utils/constData';
 
 const createSiteSortTemplate = () =>
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type='${SORT_TYPE.SORT_DAY}'>
       <label class="trip-sort__btn" for="sort-day">Day</label>
     </div>
 
@@ -13,12 +14,12 @@ const createSiteSortTemplate = () =>
     </div>
 
     <div class="trip-sort__item  trip-sort__item--time">
-      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time"  data-sort-type='${SORT_TYPE.SORT_TIME}'>
       <label class="trip-sort__btn" for="sort-time">Time</label>
     </div>
 
     <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type='${SORT_TYPE.SORT_PRICE}'>
       <label class="trip-sort__btn" for="sort-price">Price</label>
     </div>
 
